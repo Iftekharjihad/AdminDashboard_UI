9af8fb77abf57663e75995a7df15555d6831ebb5
Iftekar Islam <iftekharislam923@gmail.com>
2026-05-10 16:33:21 +0600

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard_UI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Products> Products { get; set; }
+    }
+}
